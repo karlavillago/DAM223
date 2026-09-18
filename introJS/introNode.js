3db647b01e7d412930b020bafa4cbc3692399e92
karlavillago <125054116@upq.edu.mx>
2026-09-18 16:20:18 -0600

    se hizo investigacion de de objetos, propiedades y arrays

--- introJS/introNode.js
+++ introJS/introNode.js
@@ -1,6 +1,12 @@
 console.log("Hola mundo NODE")
-let. edad1 = 19;
-let. edad2 = 21;
+let  edad1 = 19;
+let  edad2 = 21;
 
 console.log("edad promedio:");
 console.log ((edad1+edad2)/2);
+
+console.log("Medidor de procesos");
+
+console.time('Miproceso');
+for(let i=0 ;i< 100000000;i++){}
+console.timeEnd('Miproceso');
